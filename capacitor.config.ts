add77f494fac6802b25eb13660147b7ef169bae2
Todd H <github@simplybest.com>
2024-05-02 09:05:16 -0600

    add android and ios projects, upgrade favicon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'aprTabs',
+  appId: 'com.simplybest.thale.maytabs',
+  appName: 'MTech Tabs',
   webDir: 'www'
 };
 
