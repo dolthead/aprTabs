d1e7f9b27bb3e0ef9b2f5ba2e98f6e6a8d477c0e
Todd H <github@simplybest.com>
2024-05-03 09:25:57 -0600

    add share and camera native feature

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,69 @@
 import { Component, inject } from '@angular/core';
-import { AlertController, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Platform, AlertController, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Share } from '@capacitor/share';
+import { Camera } from '@capacitor/camera';
+import { Badge } from '@awesome-cordova-plugins/badge/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [ IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonButton,  IonHeader, IonToolbar, IonTitle, IonContent],
+  providers: [Badge],
 })
 export class Tab2Page {
   private alert: AlertController = inject(AlertController);
+  private platform: Platform = inject(Platform);
 
   constructor() {}
 
+  async shareNow() {
+    if (this.platform.is('hybrid')) {
+      await Share.share({
+        title: 'See cool stuff',
+        text: 'Really awesome thing you need to see right meow',
+        url: 'http://ionicframework.com/',
+        dialogTitle: 'Share with buddies',
+      });
+    } else {
+      // share with web share api
+      const { share } = navigator;
+      if (share) {
+        await share({
+          title: 'See cool stuff',
+          text: 'Really awesome thing you need to see right meow',
+          url: 'http://ionicframework.com/',
+        });
+      } else {
+        const alert = await this.alert.create({
+          header: 'Bummer',
+          message: 'Web Share API is not supported on this browser',
+          buttons: ['OK'],
+        });
+        await alert.present();
+      }
+    }
+  }
+  
+  // Share text only
+  // await Share.share({
+  //   text: 'Really awesome thing you need to see right meow',
+  // });
+  
+  // // Share url only
+  // await Share.share({
+  //   url: 'http://ionicframework.com/',
+  // });
+
+  async sharePhotos() {
+    // Share multiple files using files parameter
+    const { photos } = await Camera.pickImages({
+      quality: 90,
+      limit: 3,
+    });
+    await Share.share({
+      files: photos.map(photo => photo.path!),
+    });
+  }
 }
